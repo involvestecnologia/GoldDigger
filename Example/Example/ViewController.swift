d2f1a1b110d511363f5bd401840c368f253fede6
Felipe Lobo <felipe.lobo@involves.com.br>
2018-10-28 02:15:28 -0300

    Re-create example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Felipe Lobo on 28/10/18.
+//  Copyright © 2018 Copyisright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
